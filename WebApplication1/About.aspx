57964ecbed75251bea535bee1643b32fd07c7c8c
loicakamche <47833486+loicakamche@users.noreply.github.com>
2019-12-04 14:59:59 -0500

    test
    webapp

---
+++ WebApplication1/About.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="True" CodeBehind="About.aspx.cs" Inherits="WebApplication1.About" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: Title %>.</h2>
+    <h3>Your application description page.</h3>
+    <p>Use this area to provide additional information.</p>
+
+    <asp:TextBox ID ="txtNumber" type="text" class="form-control" runat="server" autocomplete="off" placeholder="Number to encode"/>
+    <asp:TextBox id="txtcodded" type="text" class="form-control" runat="server" autocomplete="off" placeholder="Number encoded"/>    
+    
+    <asp:Button ID="Btncode" runat="server" Text="CODE" CssClass="btn btn-primary"  onclick="btncoded_Click" />  
+
+
+    <asp:TextBox ID ="txtFirstByte" type="text" class="form-control" runat="server" autocomplete="off" placeholder="first byte to decode"/>
+    <asp:TextBox id="txtLastByte" type="text" class="form-control" runat="server" autocomplete="off" placeholder="last byte to decode"/>
+
+    <asp:TextBox id="txtdecoded" type="text" class="form-control" runat="server" autocomplete="off" placeholder="Number"/>    
+    
+    <asp:Button ID="Btndecode" runat="server" Text="DECODE" CssClass="btn btn-primary" onclick="btndecoded_Click"  />
+</asp:Content>
---
+++ WebApplication1/About.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btncoded_Click(object sender, EventArgs e)
+        {
+            string teht = encode(89);
+            txtcodded.Text = encode(int.Parse(txtNumber.Text.ToString()));
+        }
+
+        protected void btndecoded_Click(object sender, EventArgs e)
+        {
+            txtdecoded.Text = decode(txtLastByte.Text, txtFirstByte.Text);
+        }
+
+        public static string encode(int unEncoded)
+        {
+            int intermediateDec = unEncoded + 8192;
+            int firstSevenBits = intermediateDec & 0x007F; 
+            int lastSevenBits = intermediateDec & 0x3F80; 
+            int encoded = firstSevenBits + (lastSevenBits << 1);
+            return encoded.ToString("X");
+        }
+            
+        public static string decode(string lastByte, string firstByte)
+        {
+            byte last = Convert.ToByte(lastByte, 16);
+            byte first = Convert.ToByte(firstByte, 16);
+            short intermediateDec = (short)(last + (first << 7));
+            short decoded = (short)(intermediateDec - 8192);
+            return decoded.ToString();
+        }
+
+    }
+}
